ad7262a46d28fe1c9aaffb8768e3a908c12859de
CC-Cristian <crielprog@gmail.com>
2024-03-15 10:51:59 -0500

    first commint

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+
+  temperaturaActual: number = 105; // Valor inicial del termómetro
+}
